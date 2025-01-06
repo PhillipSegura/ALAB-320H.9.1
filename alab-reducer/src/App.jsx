b5f156e00e257057b5d16d7375221cd20488b50f
PhillipSegura <PhillipaSegura@gmail.com>
2025-01-06 03:22:52 -0700

    added some basic styling and fun background

--- alab-reducer/src/App.jsx
+++ alab-reducer/src/App.jsx
@@ -1,7 +1,3 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
 import React, { useReducer, useState } from "react";
 import "./App.css";
 
